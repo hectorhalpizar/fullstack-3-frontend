3653cba75b3954ebd6d8b56e71a2ceb29fa69092
Hector Alpizar <hector.alpizar.c@gmail.com>
2023-05-11 02:01:31 -0700

    Season 2 - Projects Page added

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,3 +1,35 @@
-export default function Projects() {
-    return <h1>Projects Page</h1>;
+import PageDescription from "@/components/PageDescription";
+import ProjectItem from "@/components/ProjectItem";
+
+export default function Projects({ projects }) {
+    return (
+        <section>
+            <PageDescription
+                title="Projects"
+                description="These are my projects"
+            />
+
+            { projects.map(project => <ProjectItem key={project.id} project={project} />) }
+        </section>
+    );
+}
+
+export async function getServerSideProps() {
+
+    let projects = [];
+
+    try {
+        const response = await fetch("http://172.31.16.138:3000/api/projects");
+        const data = await response.json();
+
+        projects = data;
+    } catch (error) {
+        console.error(error);
+    }
+
+    return {
+        props: {
+            projects,
+        },
+    };
 }
